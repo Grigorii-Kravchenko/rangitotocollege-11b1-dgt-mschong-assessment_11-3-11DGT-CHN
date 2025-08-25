413290a0a7bc550c64fac17b87ee399181ddc5bf
Grigorii-Kravchenko <194200@cloud.rangitoto.school.nz>
2025-08-25 15:17:19 +1200

    Modified grid and start of the saving buttons position  using function

--- Python/Memorygame.py
+++ Python/Memorygame.py
@@ -1,23 +1,30 @@
 import tkinter as tk
 root = tk.Tk()
 
+grid = [[][][]]
+
+
+
+def addToGrid(int position):
+    grid.append()
+
+
+
+
 
 
 def startGrid():
-    coordx = 0
-    
-    coordy = 0
-    for j in range(3):
+
+    c = 0 #variable for column
+    r= 0 #veriable for row
+    for j in range(9):
         square = tk.Button(root,text="",width=10,height=5,  activebackground="yellow", 
-                activeforeground="red")
-        square.pack(padx=200, pady=20)
-        
-    coordx = 200
-    for j in range(3):
-        square = tk.Button(root,text="",width=10,height=5,  activebackground="blue", 
-                activeforeground="red")
-        square.pack(padx=200, pady=20)
-        square.place(x=coordx, y = coordy)
+                activeforeground="red").grid(column = c, row = r, padx=5) #displaying in the grid
+        c +=1
+
+        if c % 3==0: #if column is divisvle by 3, therefore we need to switch the row
+            
+            r +=1 
     #square.grid(row=i, column=j)
     #square = tk.Button(root)
 startGrid()
