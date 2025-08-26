52daf71aa277f394bcadb951a68c7303fa2c864c
Grigorii-Kravchenko <194200@cloud.rangitoto.school.nz>
2025-08-27 09:48:24 +1200

    Adding a list from 0 to 9, trying to make command work

--- Python/Memorygame.py
+++ Python/Memorygame.py
@@ -1,32 +1,50 @@
 import tkinter as tk
 root = tk.Tk()
 
-grid = [[][][]]
+grid = []
+
+start = True
+for i in range(0,9):
+    grid.append(0)
 
 
-
-def addToGrid(int position):
-    grid.append()
+sequence = []
 
 
+def makeOn(position, start,finished):
+    print(start)
+    print(finished)
+    if start == False and finished == True:
+        if grid[position] != 1:
+            sequence.append(position)
+            grid[position] = 1
+            print(f"changed {position}")
+            print(sequence)
+    print(grid)
+    print("button was pressed")
 
 
 
 
 def startGrid():
-
+    start = True
+    finished = False
     c = 0 #variable for column
     r= 0 #veriable for row
-    for j in range(9):
+    counter = 0
+    for j in range(0,9):
+        
         square = tk.Button(root,text="",width=10,height=5,  activebackground="yellow", 
-                activeforeground="red").grid(column = c, row = r, padx=5) #displaying in the grid
+                activeforeground="red",command = makeOn(counter, start, finished)).grid(column = c, row = r, padx=5,) #displaying in the grid
         c +=1
-
+        counter+=1
+        start = False
         if c % 3==0: #if column is divisvle by 3, therefore we need to switch the row
-            
+            c = 0
             r +=1 
     #square.grid(row=i, column=j)
     #square = tk.Button(root)
+    finished = True
 startGrid()
 
 root.mainloop()
